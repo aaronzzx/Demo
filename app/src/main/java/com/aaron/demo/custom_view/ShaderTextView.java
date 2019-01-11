d48e553fb243a4eaf89a7cef3c66d8f29899fd4f
Aaron <aaronzheng9603@gmail.com>
2019-01-11 16:01:04 +0800

    1.探索 ShaderTextView

--- app/src/main/java/com/aaron/demo/custom_view/ShaderTextView.java
+++ app/src/main/java/com/aaron/demo/custom_view/ShaderTextView.java
@@ -10,11 +10,15 @@ import android.graphics.Paint;
 import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
 @SuppressLint("AppCompatCustomView")
 public class ShaderTextView extends TextView {
 
+    private static final String TAG = "ShaderTextView";
+    private int mSizeChanged = 0;
+    private int mOnDraw = 0;
     private int mViewWidth;
     private int mViewHeight;
     private int mTranslate;
@@ -40,31 +44,39 @@ public class ShaderTextView extends TextView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+//        Log.d(TAG, "onDraw: start execute---" + mOnDraw++);
         if (mGradientMatrix != null) {
             mTranslate += mViewWidth / 5;
+//            Log.d(TAG, "mTranslate(1): " + mTranslate);
             if (mTranslate > 2 * mViewWidth) {
                 mTranslate = -mViewWidth;
+//                Log.d(TAG, "mTranslate(2): " + mTranslate);
             }
             mGradientMatrix.setTranslate(mTranslate, 0);
             mLinearGradient.setLocalMatrix(mGradientMatrix);
-            postInvalidateDelayed(300);
+            postInvalidateDelayed(100);
         }
+//        Log.d(TAG, "onDraw: finish execute---" + mOnDraw++);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+//        Log.d(TAG, "onSizeChanged: start execute---" + mSizeChanged++);
+        Log.d(TAG, "mViewWidth(1): " + mViewWidth);
         if (mViewWidth == 0) {
             mViewWidth = getMeasuredWidth();
             mViewHeight = getMeasuredHeight();
+            Log.d(TAG, "mViewWidth(2): " + mViewWidth);
         }
         if (mViewWidth > 0) {
             Paint paint = getPaint();
             mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0,
-                    new int[]{Color.RED, Color.GREEN, Color.BLUE}, null,
-                    Shader.TileMode.CLAMP);
+                    new int[]{Color.BLUE, Color.WHITE, Color.BLUE}, null,
+                    Shader.TileMode.MIRROR);
             paint.setShader(mLinearGradient);
             mGradientMatrix = new Matrix();
         }
+//        Log.d(TAG, "onSizeChanged: finish execute---" + mSizeChanged++);
     }
 }
