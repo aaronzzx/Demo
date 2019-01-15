c1133c1b1c601ae231289edfe00efec57531586a
Aaron <aaronzheng9603@gmail.com>
2019-01-15 19:46:09 +0800

    1.恢复文件

--- app/src/main/java/com/aaron/demo/custom_view/Topbar.java
+++ app/src/main/java/com/aaron/demo/custom_view/Topbar.java
@@ -4,26 +4,32 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
+import android.view.Gravity;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.aaron.demo.R;
 
-public class Topbar extends ViewGroup {
+public class Topbar extends RelativeLayout {
 
+    private TextView mTitleView;
     private String mTitleText;
     private float mTitleTextSize;
     private int mTitleTextColor;
+    private LayoutParams mTitleParams;
 
     private Button mLeftButton;
     private String mLeftText;
     private int mLeftTextColor;
     private Drawable mLeftBackground;
+    private LayoutParams mLeftParams;
 
     private Button mRightButton;
     private String mRightText;
     private int mRightTextColor;
     private Drawable mRightBackground;
+    private LayoutParams mRightParams;
 
     public Topbar(Context context) {
         super(context);
@@ -31,34 +37,59 @@ public class Topbar extends ViewGroup {
 
     public Topbar(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initView(context, attrs);
+        initAttrs(context, attrs);
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    private void initView(Context context) {
+        // init view
+        mTitleView = new TextView(context);
+        mLeftButton = new Button(context);
+        mRightButton = new Button(context);
+
+        // set titleView's xml attr
+        mTitleView.setText(mTitleText);
+        mTitleView.setTextColor(mTitleTextColor);
+        mTitleView.setTextSize(mTitleTextSize);
+        mTitleView.setGravity(Gravity.CENTER);
+
+        // set leftButton's xml attr
+        mLeftButton.setText(mLeftText);
+        mLeftButton.setTextColor(mLeftTextColor);
+        mLeftButton.setBackground(mLeftBackground);
+
+        // set rightButton's xml attr
+        mRightButton.setText(mRightText);
+        mRightButton.setTextColor(mRightTextColor);
+        mRightButton.setBackground(mRightBackground);
+
+        // 设置控件布局属性
+        mTitleParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
+        mTitleParams.addRule(RelativeLayout.CENTER_IN_PARENT, TRUE);
+        // 添加到 ViewGroup
+        addView(mTitleView, mTitleParams);
+
+        mLeftParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
+        mLeftParams.addRule(RelativeLayout.ALIGN_PARENT_START, TRUE);
+        addView(mLeftButton, mLeftParams);
+
+        mRightParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
+        mRightParams.addRule(RelativeLayout.ALIGN_PARENT_END, TRUE);
+        addView(mRightButton, mRightParams);
     }
 
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
-    }
-
-    private void initView(Context context, AttributeSet attrs) {
-        // TopBar's title
+    private void initAttrs(Context context, AttributeSet attrs) {
+        // TopBar's attrs of title
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.Topbar);
         mTitleText = typedArray.getString(R.styleable.Topbar_titleText);
         mTitleTextSize = typedArray.getDimension(R.styleable.Topbar_titleTextSize, 18);
         mTitleTextColor = typedArray.getColor(R.styleable.Topbar_titleTextColor, 0xFFFFFFFF);
 
-        // TopBar's leftButton
-        mLeftButton = new Button(context);
+        // TopBar's attrs of leftButton
         mLeftText = typedArray.getString(R.styleable.Topbar_leftText);
         mLeftTextColor = typedArray.getColor(R.styleable.Topbar_leftTextColor, 0xFFFFFFFF);
         mLeftBackground = typedArray.getDrawable(R.styleable.Topbar_leftBackground);
 
-        // TopBar's rightButton
-        mRightButton = new Button(context);
+        // TopBar's attrs of rightButton
         mRightText = typedArray.getString(R.styleable.Topbar_rightText);
         mRightTextColor = typedArray.getColor(R.styleable.Topbar_rightTextColor, 0xFFFFFFFF);
         mRightBackground = typedArray.getDrawable(R.styleable.Topbar_rightBackground);
