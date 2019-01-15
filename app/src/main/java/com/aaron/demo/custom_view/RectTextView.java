0aa3be8439ae5c55181ac35a260a0173a59669aa
Aaron <aaronzheng9603@gmail.com>
2019-01-15 16:12:24 +0800

    1.添加注释

--- app/src/main/java/com/aaron/demo/custom_view/RectTextView.java
+++ app/src/main/java/com/aaron/demo/custom_view/RectTextView.java
@@ -37,21 +37,26 @@ public class RectTextView extends TextView {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 //        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        // 将自己测量好的宽高传递进去
         setMeasuredDimension(measureValue(widthMeasureSpec, CALC_WIDTH),
                 measureValue(heightMeasureSpec, CALC_HEIGHT));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        // 画背景
         canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaintBackground);
+        // 画边框
         canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaintStroke);
         super.onDraw(canvas);
     }
 
     private int measureValue(int measureSpec, int type) {
         int result;
+        // 获得测量模式与测量大小
         int measureMode = MeasureSpec.getMode(measureSpec);
         int measureSize = MeasureSpec.getSize(measureSpec);
+        // 根据不同的测量模式处理测量大小
         if (measureMode == MeasureSpec.EXACTLY) {
             result = measureSize;
         } else {
@@ -65,16 +70,21 @@ public class RectTextView extends TextView {
 
     private int calculateDimen(int type) {
         Paint paint = new Paint();
+        // 将字体大小设置进去
         paint.setTextSize(getTextSize());
+        // 获取字符串像素宽度
         float textWidth = paint.measureText(getText().toString());
         int result = 0;
         switch (type) {
             case CALC_WIDTH:
+                // 为宽度增加间距
                 result = (int) (textWidth + 80);
                 break;
             case CALC_HEIGHT:
+                // 为高度增加间距
                 float textSize = getTextSize();
                 result = (int) (textSize + 80);
+                // 判断行数，不严谨
                 if (textWidth >= 1080) {
                     int flag = (int) (textWidth / 1080);
                     result = (int) (textSize * flag + 180);
--- app/src/main/java/com/aaron/demo/custom_view/ShaderTextView.java
+++ app/src/main/java/com/aaron/demo/custom_view/ShaderTextView.java
@@ -15,6 +15,8 @@ import android.widget.TextView;
 @SuppressLint("AppCompatCustomView")
 public class ShaderTextView extends TextView {
 
+    private static final String TAG = "ShaderTextView";
+
     private Paint mShaderPaint;
     private LinearGradient mLinearGradient;
     private Matrix mGradientMatrix;
@@ -33,13 +35,19 @@ public class ShaderTextView extends TextView {
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         if (mViewWidth == 0) {
+            // 如果控件宽度为 0 ，那么就获取已测量的宽度并赋值
             mViewWidth = getMeasuredWidth();
             if (mViewWidth > 0) {
+                // 当控件宽度大于 0 时才允许初始化 Paint,Shader,Matrix
+                // 获取 TextView 的 TextPaint
                 mShaderPaint = getPaint();
+                // 初始化渐变规格
                 mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0,
                         new int[]{Color.RED, 0xffffffff, Color.RED}, null,
                         Shader.TileMode.CLAMP);
+                // 为画笔设置渐变属性
                 mShaderPaint.setShader(mLinearGradient);
+                // 初始化 Matrix
                 mGradientMatrix = new Matrix();
             }
         }
@@ -49,12 +57,17 @@ public class ShaderTextView extends TextView {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if (mGradientMatrix != null) {
+            // 不停改变渐变位置，每次增加的量为控件宽度的五分之一
             mTranslate += mViewWidth / 5;
             if (mTranslate > mViewWidth * 2) {
+                // 当渐变位置超过控件宽度的两倍时，使用控件宽度负值重置渐变位置
                 mTranslate = -mViewWidth;
             }
+            // 为矩阵设置位移
             mGradientMatrix.setTranslate(mTranslate, 0);
+            // 为 Shader 设置渐变效果的位移矩阵
             mLinearGradient.setLocalMatrix(mGradientMatrix);
+            // 每 0.1 秒通知系统重绘，达到闪烁效果
             postInvalidateDelayed(100);
         }
     }
