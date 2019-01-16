09a0de8ab3c808d3ac1e181338d5575df9c8c889
Aaron <aaronzheng9603@gmail.com>
2019-01-16 14:13:10 +0800

    1.观察 MotionEvent 事件传递流程

---
+++ app/src/main/java/com/aaron/demo/motion_event/MyView.java
@@ -0,0 +1,32 @@
+package com.aaron.demo.motion_event;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MyView extends View {
+
+    private static final String TAG = "MyView";
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        Log.d(TAG, "dispatchTouchEvent: " + event.getAction());
+        return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouchEvent: " + event.getAction());
+        return super.onTouchEvent(event);
+    }
+}
---
+++ app/src/main/java/com/aaron/demo/motion_event/MyViewGroupA.java
@@ -0,0 +1,43 @@
+package com.aaron.demo.motion_event;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.RelativeLayout;
+
+public class MyViewGroupA extends RelativeLayout {
+
+    private static final String TAG = "MyViewGroupA";
+
+    public MyViewGroupA(Context context) {
+        super(context);
+    }
+
+    public MyViewGroupA(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Log.d(TAG, "dispatchTouchEvent: " + ev.getAction());
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        Log.d(TAG, "onInterceptTouchEvent: " + ev.getAction());
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouchEvent: " + event.getAction());
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+    }
+}
