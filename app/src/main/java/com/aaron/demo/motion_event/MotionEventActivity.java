d7727386d6add9bd3d15380cadaa91e73e7f5d0a
Aaron <aaronzheng9603@gmail.com>
2019-01-16 12:50:35 +0800

    1.添加包 motion_event

---
+++ app/src/main/java/com/aaron/demo/motion_event/MotionEventActivity.java
@@ -0,0 +1,15 @@
+package com.aaron.demo.motion_event;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aaron.demo.R;
+
+public class MotionEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motion_event);
+    }
+}
