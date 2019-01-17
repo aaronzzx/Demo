dc852b7b5024dfa227ad6c5f244b0b5c55480b1c
Aaron <aaronzheng9603@gmail.com>
2019-01-17 14:51:48 +0800

    1.修改包 content_provider 内容

--- app/src/main/java/com/aaron/demo/content_provider/ProviderActivity.java
+++ app/src/main/java/com/aaron/demo/content_provider/ProviderActivity.java
@@ -3,13 +3,17 @@ package com.aaron.demo.content_provider;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -21,33 +25,63 @@ import java.util.List;
 /**
  * 学习 ContentProvider
  */
-public class ProviderActivity extends AppCompatActivity {
+public class ProviderActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "ProviderActivity";
     private static final int REQUEST_PERMISSION = 1;
-    private ArrayAdapter mAdapter;
-    private List<String> mContactsList = new ArrayList<>();
+    private static final int READ_CONTACTS = 11;
+    private static final int READ_MESSAGES = 12;
+    private BaseAdapter mAdapter;
+    private List<String> mStringList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider);
         initView();
-        checkPermission();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_read_contacts:
+                checkPermission(Manifest.permission.READ_CONTACTS, READ_CONTACTS);
+                break;
+            case R.id.btn_read_messages:
+                checkPermission(Manifest.permission.READ_SMS, READ_MESSAGES);
+                break;
+            case R.id.btn_clear_list:
+                mStringList.clear();
+                mAdapter.notifyDataSetChanged();
+                break;
+        }
     }
 
     private void initView() {
+        Button readContacts = findViewById(R.id.btn_read_contacts);
+        Button readMessages = findViewById(R.id.btn_read_messages);
+        Button clearList = findViewById(R.id.btn_clear_list);
+        readContacts.setOnClickListener(this);
+        readMessages.setOnClickListener(this);
+        clearList.setOnClickListener(this);
         ListView listView = findViewById(R.id.list_view);
-        mAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mContactsList);
+        mAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mStringList);
         listView.setAdapter(mAdapter);
     }
 
-    private void checkPermission() {
-        if (ContextCompat.checkSelfPermission(ProviderActivity.this, Manifest.permission.READ_CONTACTS)
+    private void checkPermission(String permission, int type) {
+        // 检查权限
+        if (ContextCompat.checkSelfPermission(this, permission)
                 != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(ProviderActivity.this,
-                    new String[]{Manifest.permission.READ_CONTACTS}, REQUEST_PERMISSION);
+            // 申请权限
+            ActivityCompat.requestPermissions(this,
+                    new String[]{permission}, REQUEST_PERMISSION);
         } else {
-            readContacts();
+            if (type == READ_CONTACTS) {
+                readContacts();
+            } else if (type == READ_MESSAGES) {
+                readMessages();
+            }
         }
     }
 
@@ -56,8 +90,13 @@ public class ProviderActivity extends AppCompatActivity {
                                            @NonNull int[] grantResults) {
         switch (requestCode) {
             case REQUEST_PERMISSION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    readContacts();
+                if (grantResults.length > 0 && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED) {
+//                    if (permissions[0].equals(Manifest.permission.READ_CONTACTS)) {
+//                        readContacts();
+//                    } else if (permissions[0].equals(Manifest.permission.READ_SMS)) {
+//                        readMessages();
+//                    }
                 } else {
                     Toast.makeText(ProviderActivity.this, "请先打开权限",
                             Toast.LENGTH_SHORT).show();
@@ -77,7 +116,29 @@ public class ProviderActivity extends AppCompatActivity {
                             .CommonDataKinds.Phone.DISPLAY_NAME));
                     String number = cursor.getString(cursor.getColumnIndex(ContactsContract
                             .CommonDataKinds.Phone.NUMBER));
-                    mContactsList.add(displayName + "\n" + number);
+                    mStringList.add(displayName + "\n" + number);
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    private void readMessages() {
+        Cursor cursor = null;
+        try {
+            cursor = getContentResolver().query(Uri.parse("content://sms/"),
+                    new String[]{"_id", "address", "date", "body", "type"}, null, null, null);
+            if (cursor != null) {
+                while (cursor.moveToNext()) {
+                    String address = cursor.getString(1);
+                    String body = cursor.getString(3);
+                    mStringList.add(address + "\n" + body);
                 }
                 mAdapter.notifyDataSetChanged();
             }
