987128b00bcb7c06d654eb0d0b4ef31f20c397c4
Aaron <aaronzheng9603@gmail.com>
2019-01-19 12:14:06 +0800

    1.在 2D 绘图示例中多添加了 11 张图片，并增加点击图片切换下一张图片的功能

--- app/src/main/java/com/aaron/demo/xml_paint/XmlPaintActivity.java
+++ app/src/main/java/com/aaron/demo/xml_paint/XmlPaintActivity.java
@@ -9,7 +9,6 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
@@ -18,12 +17,20 @@ import com.aaron.demo.R;
 public class XmlPaintActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     private static final String TAG = "XmlPaintActivity";
-    private static final int MID_VALUE = 50;
+    private static final int MID_VALUE = 50; // SeekBar 中间值
     private ImageView mImageView;
     private Bitmap mBitmap;
-    private float mHue = 1.0F;
+    private static final int[] mImgArray = {R.drawable.img_1, R.drawable.img_2,
+            R.drawable.img_3, R.drawable.img_4, R.drawable.img_5, R.drawable.img_6,
+            R.drawable.img_8, R.drawable.img_9, R.drawable.img_10, R.drawable.img_11,
+            R.drawable.img_12, R.drawable.img_13};
+    private SeekBar mHueSeekBar;
+    private SeekBar mSaturationSeekBar;
+    private SeekBar mLumSeekBar;
     private float mSaturation = 1.0F;
     private float mLum = 1.0F;
+    private float mHue = 0;
+    private int mImgSelect = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,15 +49,33 @@ public class XmlPaintActivity extends AppCompatActivity implements SeekBar.OnSee
         mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.img_1);
         mImageView.setImageBitmap(mBitmap);
 
-        SeekBar hueSeekBar = findViewById(R.id.seek_hue);
-        SeekBar saturationSeekBar = findViewById(R.id.seek_saturation);
-        SeekBar lumSeekBar = findViewById(R.id.seek_lum);
+        mHueSeekBar = findViewById(R.id.seek_hue);
+        mSaturationSeekBar = findViewById(R.id.seek_saturation);
+        mLumSeekBar = findViewById(R.id.seek_lum);
 
-        hueSeekBar.setOnSeekBarChangeListener(this);
-        saturationSeekBar.setOnSeekBarChangeListener(this);
-        lumSeekBar.setOnSeekBarChangeListener(this);
+        mHueSeekBar.setOnSeekBarChangeListener(this);
+        mSaturationSeekBar.setOnSeekBarChangeListener(this);
+        mLumSeekBar.setOnSeekBarChangeListener(this);
+        mImageView.setOnClickListener(v -> {
+            mBitmap = BitmapFactory.decodeResource(getResources(), getImgResource());
+            mImageView.setImageBitmap(mBitmap);
+            mHueSeekBar.setProgress(50);
+            mSaturationSeekBar.setProgress(50);
+            mLumSeekBar.setProgress(50);
+        });
     }
 
+    private int getImgResource() {
+        if (mImgSelect == mImgArray.length - 1) {
+            mImgSelect = -1;
+        }
+        mImgSelect++;
+        return mImgArray[mImgSelect];
+    }
+
+    /**
+     * 随着手指在 SeekBar 上移动而回调
+     */
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         switch (seekBar.getId()) {
@@ -67,10 +92,19 @@ public class XmlPaintActivity extends AppCompatActivity implements SeekBar.OnSee
 //        mImageView.setImageBitmap(handleImageEffect(mBitmap, mHue, mSaturation, mLum));
     }
 
+    /**
+     * 图像处理
+     *
+     * @param bitmap     要修改的原图
+     * @param hue        需要调整的色相的值
+     * @param saturation 需要调整的饱和度的值
+     * @param lum        需要调整的亮度的值
+     * @return 由于 Android 不允许直接修改原图，因此需要返回一个已经修改的 Bitmap
+     */
     private Bitmap handleImageEffect(Bitmap bitmap, float hue, float saturation, float lum) {
-        Log.d(TAG, "hue: " + hue);
-        Log.d(TAG, "saturation: " + saturation);
-        Log.d(TAG, "lum: " + lum);
+//        Log.d(TAG, "hue: " + hue);
+//        Log.d(TAG, "saturation: " + saturation);
+//        Log.d(TAG, "lum: " + lum);
         Bitmap bmp = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bmp);
         Paint paint = new Paint();
@@ -96,22 +130,29 @@ public class XmlPaintActivity extends AppCompatActivity implements SeekBar.OnSee
         return bmp;
     }
 
+    /**
+     * 手指接触 SeekBar 时会回调
+     */
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
 
     }
 
+    /**
+     * 手指离开 SeekBar 时会回调
+     */
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
+        int progress = seekBar.getProgress();
         switch (seekBar.getId()) {
             case R.id.seek_hue:
-                mHue = (seekBar.getProgress() - MID_VALUE) * 1.0F / MID_VALUE * 180;
+                mHue = (progress - MID_VALUE) * 1.0F / MID_VALUE * 180;
                 break;
             case R.id.seek_saturation:
-                mSaturation = seekBar.getProgress() * 1.0F / MID_VALUE;
+                mSaturation = progress * 1.0F / MID_VALUE;
                 break;
             case R.id.seek_lum:
-                mLum = seekBar.getProgress() * 1.0F / MID_VALUE;
+                mLum = progress * 1.0F / MID_VALUE;
                 break;
         }
         mImageView.setImageBitmap(handleImageEffect(mBitmap, mHue, mSaturation, mLum));
